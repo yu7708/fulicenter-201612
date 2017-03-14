9a4f869e410f377dc215daa8480d718b93062ca3
yu7708 <3173571032@qq.com>
2017-03-14 16:43:28 +0800

    添加butterknife依赖

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -1,19 +1,38 @@
 package cn.ucai.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnNewGoods)
+    RadioButton btnNewGoods;
+    @BindView(R.id.btnBoutique)
+    RadioButton btnBoutique;
+    @BindView(R.id.btnCategory)
+    RadioButton btnCategory;
+    @BindView(R.id.btnCart)
+    RadioButton btnCart;
+    @BindView(R.id.btnMy)
+    RadioButton btnMy;
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
-    public void onCheckedChange(View view){
+
+    public void onCheckedChange(View view) {
 
     }
 }
