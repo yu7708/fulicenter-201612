93a687004d96891d81f5e25b8c42e3fd46ac92f3
yu7708 <3173571032@qq.com>
2017-03-18 14:58:13 +0800

    完善了一点排序

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildActivity.java
@@ -3,7 +3,9 @@ package cn.ucai.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RelativeLayout;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
@@ -11,17 +13,21 @@ import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.ui.fragment.NewGoodsFragment;
 
 public class CategoryChildActivity extends AppCompatActivity {
+    boolean change;
     boolean sortPrice;
     boolean sortAddTime;
-    int sortBy= I.SORT_BY_ADDTIME_DESC;//当前的排序
+    int sortBy = I.SORT_BY_ADDTIME_DESC;//当前的排序
 
     NewGoodsFragment newGoodsFragment;
+    @BindView(R.id.sss)
+    RelativeLayout sss;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_child);
         ButterKnife.bind(this);
-        newGoodsFragment=new NewGoodsFragment();
+        newGoodsFragment = new NewGoodsFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, newGoodsFragment).commit();
     }
@@ -35,14 +41,20 @@ public class CategoryChildActivity extends AppCompatActivity {
     public void sortList(View view) {//如何才能得到升降序呢,设置标志位
         switch (view.getId()) {
             case R.id.btn_sort_price:
-                sortBy=sortPrice?I.SORT_BY_PRICE_ASC:I.SORT_BY_PRICE_DESC;
-                sortPrice=!sortPrice;
+                sortBy = sortPrice ? I.SORT_BY_PRICE_ASC : I.SORT_BY_PRICE_DESC;
+                sortPrice = !sortPrice;
                 break;
             case R.id.btn_sort_addTime:
-                sortBy=sortAddTime?I.SORT_BY_ADDTIME_ASC:I.SORT_BY_ADDTIME_DESC;
-                sortAddTime=!sortAddTime;
+                sortBy = sortAddTime ? I.SORT_BY_ADDTIME_ASC : I.SORT_BY_ADDTIME_DESC;
+                sortAddTime = !sortAddTime;
                 break;
         }
         newGoodsFragment.sortBy(sortBy);
+    }
+
+    @OnClick(R.id.tvChange)
+    public void onChange() {
+        sss.setVisibility(change?View.GONE:View.VISIBLE);
+        change =!change;
     }
 }
