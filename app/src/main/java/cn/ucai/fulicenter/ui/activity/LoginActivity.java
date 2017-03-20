3b39512008ee7ea3a7f041e7fbfce1ee9727b9e7
yu7708 <3173571032@qq.com>
2017-03-20 22:42:37 +0800

    登录判断,

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -12,7 +13,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.net.IUserModel;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.net.UserModel;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
+import cn.ucai.fulicenter.model.utils.ResultUtils;
 import cn.ucai.fulicenter.ui.view.MFGT;
 
 /**
@@ -24,11 +33,14 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.et_password)
     EditText etPassword;
 
+    String userName,password;
+    IUserModel mModel;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mModel=new UserModel();
     }
 
     @OnClick({R.id.backArea, R.id.btn_login, R.id.btn_register})
@@ -46,6 +58,55 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
     private void login() {
+        if(checkInput()){
+            mModel.login(LoginActivity.this, userName, password, new OnCompleteListener<String>() {
+                @Override
+                public void onSuccess(String r) {
+                    Result result = ResultUtils.getResultFromJson(r, User.class);
+                    if(result!=null&&result.isRetMsg()){
+                        User user= (User) result.getRetData();
+                        if(user!=null){
+                            loginSuccess(user);
+                        }
+                    }else{
+                        if(result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
+                            CommonUtils.showShortToast(R.string.login_fail_unknow_user);
+                        }
+                        if(result.getRetCode()==I.MSG_LOGIN_ERROR_PASSWORD){
+                            CommonUtils.showShortToast(R.string.login_fail_error_password);
+                        }
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    CommonUtils.showShortToast(R.string.login_fail);
+                }
+            });
+        }
+    }
+
+    private void loginSuccess(User user) {
+        FuLiCenterApplication.setCurrentUser(user);
+    }
+
+    private boolean checkInput() { if(TextUtils.isEmpty(userName)){
+        etUsername.requestFocus();
+        etUsername.setError(getString(R.string.user_name_connot_be_empty));
+        return false;
+    }
+        if(!userName.matches("[a-zA-z]\\w{5,15}")){//判断违法字符,取6到16位,取非,判断不是所给情况
+            etUsername.requestFocus();
+            etUsername.setError(getString(R.string.illegal_user_name));
+            return false;
+        }
+        if(TextUtils.isEmpty(password)){
+            etPassword.requestFocus();
+            etPassword.setError(getString(R.string.password_connot_be_empty));
+            return false;
+        }
+
+        return true;
     }
 
     @Override
