b481ac4a558c97f52b13107250b59df124cf014d
yu7708 <3173571032@qq.com>
2017-03-21 17:57:48 +0800

    闪屏界面获取数据库里的用户数据

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.FuLiCenterApplication;
+import cn.ucai.fulicenter.model.bean.User;
+import cn.ucai.fulicenter.model.dao.UserDao;
+import cn.ucai.fulicenter.model.utils.SharedPreferenceUtils;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
     int time=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,12 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                String username= SharedPreferenceUtils.getInstance().getUserName();
+                if(username!=null){//判断是否登录过,提取信息
+                    User user= UserDao.getInstance(SplashActivity.this).getUserInfo(username);
+                    Log.e(TAG,"username="+user);
+                    FuLiCenterApplication.setCurrentUser(user);//每次onDestory,Application都会清空
+                }
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 SplashActivity.this.finish();
             }
